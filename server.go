54b3fc2fca7214057ad1c8f9f979976d502a2f72
Zenno Zenith <95485961+ZennoZenith@users.noreply.github.com>
2026-03-15 17:21:06 +0530

    solved the alpinejs hx-post issue

--- server.go
+++ server.go
@@ -18,20 +18,10 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-type OutputMessage struct {
-	Dir_porfiles []models.Dirprofile `json:"dir_porfiles"`
-	Error        bool                `json:"error"`
-}
 type InputMessage struct {
 	Path string `json:"path"`
 }
 
-func sendOutput(conn *websocket.Conn, dir_porfiles []models.Dirprofile, isError bool) {
-	msg := OutputMessage{Dir_porfiles: dir_porfiles, Error: isError}
-	data, _ := json.Marshal(msg)
-	fmt.Println(string(data))
-	conn.WriteMessage(websocket.TextMessage, data)
-}
 func filelist(path string) ([]fs.DirEntry, error) {
 	dir := "/home/zenith/meeran/cloudIDE/user1"
 	subpath := dir + "/" + path
@@ -65,7 +55,6 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		var input InputMessage
 		if err := json.Unmarshal(msg, &input); err != nil {
 			fmt.Println("error")
-			sendOutput(conn, []models.Dirprofile{}, true)
 			continue
 		}
 
@@ -79,37 +68,48 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Client disconnected")
 }
+
+func browseHandler(w http.ResponseWriter, r *http.Request) {
+	output, _ := filelist("fold")
+	models.DirprofileArr = nil
+	for _, entry := range output {
+		models.DirprofileArr = append(models.DirprofileArr, models.Dirprofile{Name: entry.Name(), IsDir: entry.IsDir()})
+	}
+	components.FileStructure(models.DirprofileArr).Render(r.Context(), w)
+	models.DirprofileArr = nil
+}
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	models.DirprofileArr = nil
+	if len(models.DirprofileArr) != 0 {
+		component := components.Base(models.DirprofileArr)
+		component.Render(r.Context(), w)
+		return
+	}
+	dir := "/home/zenith/meeran/cloudIDE/user1"
+	root := os.DirFS(dir)
+
+	entries, err := fs.ReadDir(root, ".")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		models.DirprofileArr = append(models.DirprofileArr, models.Dirprofile{Name: entry.Name(), IsDir: entry.IsDir()})
+	}
+	components.Base(models.DirprofileArr).Render(r.Context(), w)
+	models.DirprofileArr = nil
+
+}
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		models.DirprofileArr = nil
-		if len(models.DirprofileArr) != 0 {
-			fmt.Println("wo")
-			component := components.Base(models.DirprofileArr)
-			component.Render(r.Context(), w)
-			return
-		}
-		dir := "/home/zenith/meeran/cloudIDE/user1"
-		root := os.DirFS(dir)
-
-		entries, err := fs.ReadDir(root, ".")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, entry := range entries {
-			models.DirprofileArr = append(models.DirprofileArr, models.Dirprofile{Name: entry.Name(), IsDir: entry.IsDir()})
-		}
-		components.Base(models.DirprofileArr).Render(r.Context(), w)
-		models.DirprofileArr = nil
-
-	})
-	mux.HandleFunc("GET /ws", wsHandler)
+	mux.HandleFunc("GET /", homepageHandler)
+	// mux.HandleFunc("POST /ws", wsHandler)
+	mux.HandleFunc("POST /browse", browseHandler)
 	mux.Handle("GET /public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	fmt.Println("🚀  GOTTH Todo → http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
